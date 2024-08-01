acb6f56a590703a5dbfb960591e19c8742c46fec
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-07-31 17:44:34 -0700

    c printf project

--- _printf.c
+++ _printf.c
@@ -7,7 +7,6 @@
  */
 int _printf(const char *format, ...)
 {
-
 	convert_match m[] = {
 		{"%s", print_string}, {"%c", printf_charac},
 		{"%%", printf_percent}
@@ -20,32 +19,37 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0')) {
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
 		va_end(args);
 		return (-1);
 	}
-
-	while (format[i] != '\0') {
-		if (format[i] == '%' && format[i + 1] != '\0') {
-			for (j = 0; j < m_size; j++) {
-				if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1]) {
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			for (j = 0; j < m_size; j++)
+			{
+				if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+				{
 					len += m[j].f(args);
 					i += 2;
 					break;
 				}
 			}
-			if (j == m_size) {
+			if (j == m_size)
+			{
 				_write('%');
 				len++;
 				i++;
 			}
-		} else {
+		}
+		else {
 			_write(format[i]);
 			len++;
 			i++;
 		}
 	}
-
 	va_end(args);
 	return len;
 }
